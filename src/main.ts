3acd073b152cd149a3328a7d1467d6801c0eb282
Vivek Raj <vivek.raj@brainvire.com>
2022-12-05 12:08:48 +0530

    manual route testing

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({
-        whitelist:true,
+        whitelist:true,   // restricts additional property from body eg.:- with email & password and additional property admine:true , this will trstrict this admin property
   }),
 );
   await app.listen(3000);
--- src/users/dtos/create-user.dto.ts
+++ src/users/dtos/create-user.dto.ts
@@ -2,8 +2,8 @@ import { IsEmail , IsString } from "class-validator"
 
 export class CreateUserDto{
     @IsEmail()
-    email:string ;
-
+    email:String ;
+    
     @IsString()
-    password:string;
+    password:String;
 }
