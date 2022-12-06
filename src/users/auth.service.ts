adc1d462b95eb9a03bb452972ab39f7d4b19c3b7
Vivek Raj <vivek.raj@brainvire.com>
2022-12-06 15:05:18 +0530

    Reminder on service setup

---
+++ src/users/auth.service.ts
@@ -0,0 +1,8 @@
+import { Injectable } from "@nestjs/common";
+import { UsersService } from "./users.service";
+
+@Injectable()
+export class AuthService{
+    constructor(private userService:UsersService){}
+
+}
