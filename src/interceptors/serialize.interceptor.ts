7b59e326efdb21ed81104466e63f8d67703827b3
Vivek Raj <vivek.raj@brainvire.com>
2022-12-06 12:20:32 +0530

    Customizing the interceptor's DTO

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -7,15 +7,17 @@ import {
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { plainToClass } from 'class-transformer';
-import { UserDto } from '../users/dtos/user.dto'
+
 
 export class SerializerInterceptor implements NestInterceptor{
+
+    constructor(private dto:any){}
 
     intercept(context:ExecutionContext , handler:CallHandler):Observable<any>{
         
         return handler.handle().pipe(
             map((data:any)=>{
-                return plainToClass(UserDto , data , {
+                return plainToClass(this.dto , data , {
                 excludeExtraneousValues:true, 
                 })
             }),
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -15,6 +15,7 @@ import {
   import { CreateUserDto } from './dtos/create-user.dto';
   import { UsersService } from './users.service';
   import { SerializerInterceptor } from 'src/interceptors/serialize.interceptor';
+  import { UserDto } from './dtos/user.dto';
   
   @Controller('auth')
   export class UsersController {
@@ -25,7 +26,7 @@ import {
       this.usersService.create(body.email, body.password);
     }
 
-    @UseInterceptors(SerializerInterceptor)
+    @UseInterceptors(new SerializerInterceptor(UserDto))
     @Get('/:id')
     async findUser(@Param('id') id: string) {
       console.log('handler is running')
