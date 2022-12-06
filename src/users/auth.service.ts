33e97d709c662c844fb5ed79a7183124c3468ad1
Vivek Raj <vivek.raj@brainvire.com>
2022-12-06 18:55:51 +0530

    salting and hashing the passsword

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,6 +1,11 @@
 import { BadRequestException, Injectable } from "@nestjs/common";
 import { UsersService } from "./users.service";
+import { randomBytes , scrypt as _scrypt} from "crypto";  //randomBytes is going to generate our salt & scrypt is actual hashing function
+import { promisify } from "util";  //  promisify is a function that will take in a function that makes use of callback 
+                                   //  and it's going to give us back a version of that exact same function that instead makes use of promises 
 
+ const scrypt = promisify(_scrypt);
+                                   
 @Injectable()
 export class AuthService{
     constructor(private userService:UsersService){}
@@ -14,6 +19,25 @@ export class AuthService{
               }
 
         // Hash the user password
+        //Generate a salt(salt:- randomly generated number) 
+
+        const salt = randomBytes(8).toString('hex');  //the randomBytes is going to return a buffer similar to array
+                                                      //The difference is that a buffer holdes some raw data inside of it(in the form of 0 & 1)
+                                                      //so we are going to take all that ones and zeros and turn it into a heaxadecimal string  ,
+                                                      //which  basically means a bunch of random numbers and letters
+                                                      // The 8 right here means that our buffer is going to have eight bytes
+                                                      // Every one bit of data turns into two characters when we convert into hex.
+                                                      // So our Hex is going to be a 16 character long string
+
+
+        //Hash the salt and the password together
+        const hash = (await scrypt(password , salt , 32)) as Buffer;  // 32 means just give us back 32 characters or 32 bytes worth
+
+        // join the hash result and the salt together(store in DB)
+        const result = salt + '.' + hash.toString('hex');  // here '.' is a seperator b/w salt and hash (the seperator can be any thing)
+
+
+
 
         // create a new user and save it
 
