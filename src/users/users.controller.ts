1d97c8d6fde4394065c9f694fce358a846039f14
Vivek Raj <vivek.raj@brainvire.com>
2022-12-07 16:20:14 +0530

    signing in a user

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -28,24 +28,17 @@ import {
       private authService:AuthService
       ) {}
   
-    @Get('/colors/:color')
-    setColor(@Param('color') color:string  , @Session() session:any){
-      session.color = color;
-    }
-
-    @Get('/colors')
-    getColor(@Session() session:any){
-      return session.color;
-    }
-
     @Post('/signup')
-    createUser(@Body() body: CreateUserDto) {
-      return this.authService.signup(body.email , body.password);
+     async createUser(@Body() body: CreateUserDto , @Session() session:any) {
+      const user = await this.authService.signup(body.email , body.password);
+      session.userId = user.id;
+      return user;
     }
 
     @Post('/signin')
-    signin(@Body() body:CreateUserDto){
-      return this.authService.signin(body.email , body.password);
+     async signin(@Body() body:CreateUserDto){
+      const user =  await this.authService.signin(body.email , body.password);
+      return user;
     }
 
     // @UseInterceptors(new SerializerInterceptor(UserDto))
