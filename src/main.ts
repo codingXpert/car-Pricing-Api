1ab7384d63e4585278f6976df144e823f31fafd9
Vivek Raj <vivek.raj@brainvire.com>
2022-12-07 15:05:32 +0530

    Setting up session

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys:['asdfasfd']    // this key is going to be used to encrypt the information that is stored inside the cookie 
+  }))
   app.useGlobalPipes(new ValidationPipe({
         whitelist:true,   // restricts additional property from body eg.:- with email & password and additional property admine:true , this will trstrict this admin property
   }),
