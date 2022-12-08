5a1a51c20bb2866d648145d481e16d3607bf5cef
Vivek Raj <vivek.raj@brainvire.com>
2022-12-08 17:18:06 +0530

    Improving File Layout

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -1,27 +1,33 @@
 import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
-import { User } from './user.entity'
+import { User } from './user.entity';
 
-it('can create an instance of auth service' , async () => {
-    //create a fake copy of users service
+describe('AuthService', () => {
+  let service: AuthService;
+
+  beforeEach(async () => {
+    // Create a fake copy of the users service
     const fakeUsersService: Partial<UsersService> = {
-    find:() => Promise.resolve([]),
-    create:(email:string , password:string) => Promise.resolve({id:1 , email , password} as User),
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
     };
 
-    //  const arr = await fakeUsersService.find();
-     const module = await Test.createTestingModule({
-        providers:[
-            AuthService , 
-            {
-                provide:UsersService,              // creating a DI container with this block of code.
-                useValue:fakeUsersService
-            }
-        ]                           
-     }).compile(); 
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
+    }).compile();
 
-     const service = module.get(AuthService);
+    service = module.get(AuthService);
+  });
 
-     expect(service).toBeDefined();
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
 });
