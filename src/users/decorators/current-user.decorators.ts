c9f15f5380824819706627ecc9af26b6e12b1ac6
Vivek Raj <vivek.raj@brainvire.com>
2022-12-08 11:57:35 +0530

    Why Decorators & Interceptors

--- src/users/decorators/current-user.decorators.ts
+++ src/users/decorators/current-user.decorators.ts
@@ -1,10 +1,13 @@
 import {
     createParamDecorator,
     ExecutionContext
-} from '@nestjs/common'
+} from '@nestjs/common';
 
 export const CurrentUser = createParamDecorator(
-    (data:any , context:ExecutionContext)=>{       // the execution context is the upcoming request
+    (data:never , context:ExecutionContext)=>{       // the execution context is the upcoming request
+        const request = context.switchToHttp().getRequest();
+    console.log(request.session.userId);
+    
           return 'hi there!'
     }
 )
