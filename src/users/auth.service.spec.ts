a0eb5df626598af7d7802ea749363a94de040995
Vivek Raj <vivek.raj@brainvire.com>
2022-12-08 18:48:34 +0530

    Changing Mock Implementation

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { Test } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 describe('AuthService', () => {
   let service: AuthService;
@@ -42,14 +42,9 @@ describe('AuthService', () => {
     expect(hash).toBeDefined();
   });
 
-  it('throws an error if user signs up with email that is in use', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
-    
-      await expect (service.signup('asdf@asdf.com', 
-      'asdf')).rejects.toThrow(
-        BadRequestException,
-      );
-    
+  it('throws if signin is called with an unused email', async () => {
+    await expect(
+      service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
+    ).rejects.toThrow(NotFoundException);
   });
 });
