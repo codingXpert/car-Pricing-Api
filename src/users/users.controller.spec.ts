f3b66038c8927fecbb27cae2532a57cdfc232e75
Vivek Raj <vivek.raj@brainvire.com>
2022-12-09 16:24:43 +0530

    Not Super Effective Tests

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -4,42 +4,43 @@ import { UsersService } from './users.service';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
 
-
 describe('UsersController', () => {
   let controller: UsersController;
-  let fakeUsersService:Partial<UsersService>;
-  let fakeAuthService:Partial<AuthService>;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
-
     fakeUsersService = {
-       findOne: (id:number) => {
-        return Promise.resolve({id, email:'asdf@asdf.com' , password:'asdf' } as User);
-       },
-       find:(email:string) => {
-        return Promise.resolve([{id:1 , email , password:'asdf'} as User])
-       },
-      //  remove:() => {},
-      //  update:() => {}
+      findOne: (id: number) => {
+        return Promise.resolve({
+          id,
+          email: 'asdf@asdf.com',
+          password: 'asdf',
+        } as User);
+      },
+      find: (email: string) => {
+        return Promise.resolve([{ id: 1, email, password: 'asdf' } as User]);
+      },
+      // remove: () => {},
+      // update: () => {},
     };
-
     fakeAuthService = {
       // signup: () => {},
-      // signin: () => {}
+      // signin: () => {},
     };
 
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
-      providers:[
+      providers: [
         {
           provide: UsersService,
-          useValue:fakeUsersService                  // whenever s\some one asks for user service , give them the value fakeUserService
+          useValue: fakeUsersService,
         },
         {
-          provide:AuthService,
-          useValue:fakeAuthService
-        }
-      ]
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -47,5 +48,25 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1');
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async (done) => {
+    fakeUsersService.findOne = () => null;
+    try {
+      await controller.findUser('1');
+    } catch (err) {
+      done();
+    }
   });
 });
