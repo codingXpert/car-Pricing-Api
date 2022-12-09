88c2ee545aab244b34d90e6c8db3d024a585f4dc
Vivek Raj <vivek.raj@brainvire.com>
2022-12-09 12:55:02 +0530

    More Intelligent Mocks

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -10,10 +10,18 @@ describe('AuthService', () => {
 
   beforeEach(async () => {
     // Create a fake copy of the users service
+    const users : User[] =[];
     fakeUsersService = {
-      find: () => Promise.resolve([]),
-      create: (email: string, password: string) =>
-        Promise.resolve({ id: 1, email, password } as User),
+      find:(email:string) =>{
+        const filteredUser = users.filter(user => user.email === email);
+        return Promise.resolve(filteredUser)
+      } ,
+      create: (email: string, password: string) => {
+      const user = ({id:Math.floor(Math.random() * 999999) , email , password} as User); 
+      users.push(user);
+      return Promise.resolve(user);
+      }
+        
     };
 
     const module = await Test.createTestingModule({
@@ -42,6 +50,17 @@ describe('AuthService', () => {
     expect(hash).toBeDefined();
   });
 
+
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    await service.signup('asdf@asdf.com', 'asdf');
+    await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(
+      BadRequestException,
+    );
+  });
+ 
+
+
   it('throws if signin is called with an unused email', async () => {
     await expect(
       service.signin('asdflkj@asdlfkj.com', 'passdflkj'),
@@ -61,16 +80,10 @@ describe('AuthService', () => {
 
 
 
-  
+
   it('returns a user if correct password is provided', async () => {
-    fakeUsersService.find = () =>
-      Promise.resolve([
-        {
-          email: 'asdf@asdf.com',
-          password:
-            '1f3f1f78b727fa56.af7186ab0d0a8e9cf1ae2c5c179784947a2fe349d3a3e82f6422c0d7cbc2f925',
-        } as User,
-      ]);
+   
+    await service.signup('asdf@asdf.com' , 'mypassword')
 
     const user = await service.signin('asdf@asdf.com', 'mypassword');
     expect(user).toBeDefined();
