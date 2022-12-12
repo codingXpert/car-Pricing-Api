0a07eed5dc21fadf88ef3fabdee616fc4ef74579
Vivek Raj <vivek.raj@brainvire.com>
2022-12-12 18:22:14 +0530

    126. Receiving Report Creation Requests

--- src/reports/dtos/create-report.dto.ts
+++ src/reports/dtos/create-report.dto.ts
@@ -1,15 +1,37 @@
+import {  IsString,
+    IsNumber,
+    Min,
+    Max,
+    IsLongitude,
+    IsLatitude, 
+} from "class-validator";
+
 export class CreateReportsDto{
+
+    @IsString()
     make : string;
 
+    @IsString()
     model : string;
 
+    @IsNumber()
+    @Min(1930)
+    @Max(2050)
     year: number;
 
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
     mileage : number;
 
+    @IsLongitude()
     lan : number;
 
+    @IsLatitude()
     lat : number;
 
+    @IsNumber()
+    @Min(0)
+    @Max(1000000)
     price : number;
 }
--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,9 +1,13 @@
-import { Controller , Post , Body} from '@nestjs/common';
-
+import { Controller , Post , Body , UseGuards} from '@nestjs/common';
+import { CreateReportsDto } from './dtos/create-report.dto';
+import { ReportsService } from './reports.service';
+import { AuthGuard } from 'src/guards/auth.guard';
 @Controller('reports')
 export class ReportsController {
+    constructor(private reportsService:ReportsService){}
     @Post()
-    createReport(@Body() body: CeateReportDto){
-        
+    @UseGuards(AuthGuard)
+    createReport(@Body() body: CreateReportsDto){
+       return this.reportsService.create(body);
     }
 }
