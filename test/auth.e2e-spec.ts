f3bc74557456c4aae81546486e81bb40bcb3f8a4
Vivek Raj <vivek.raj@brainvire.com>
2022-12-12 17:03:51 +0530

    122. A Followup Test

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -28,4 +28,22 @@ describe('Authentication System', () => {
         expect(email).toEqual(email);
       });
   });
+
+  it('signup as a new user then get the currently logged in user' , async () => {
+        const email = 'asdf@asdf.com';
+
+        const res = await request(app.getHttpServer())
+        .post('/auth/signup')
+        .send({email , password:'asdf'})
+        .expect(201)
+
+        const cookie = res.get('Set-Cookie');
+
+        const { body } = await request(app.getHttpServer())
+        .get('/auth/whoami')
+        .set('Cookie' , cookie)
+        .expect(200)
+
+        expect(body.email).toEqual(email);
+  });
 });
