3866e263b297a3359c5e85d5c60c8388b82c693a
Vivek Raj <vivek.raj@brainvire.com>
2022-12-14 11:49:38 +0530

    127. Saving a Report with the Reports Service

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -1,4 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Report } from './report.entity';
+import { CreateReportDto } from './dtos/create-report.dto';
+
 
 @Injectable()
-export class ReportsService {}
+export class ReportsService {
+    constructor(@InjectRepository(Report) private repo:Repository<Report> ){}
+    create(reportDto : CreateReportDto){
+        const report = this.repo.create(reportDto);
+        return this.repo.save(report);
+    } 
+}
+
