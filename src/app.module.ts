4593a0583d61c195c6765859e839f359eeda8455
Vivek Raj <vivek.raj@brainvire.com>
2022-12-19 12:29:28 +0530

    153. Providing the Cookie Key

--- src/app.module.ts
+++ src/app.module.ts
@@ -48,11 +48,16 @@ const cookieSession = require('cookie-session');
   ],
 })
 export class AppModule {
+
+constructor(private configService : ConfigService){}
+
+
+
   configure(consumer: MiddlewareConsumer) {
     consumer
       .apply(
         cookieSession({
-          keys: ['asdfasfd'],
+          keys: [this.configService.get('COOKIE_KEY')],
         }),
       )
       .forRoutes('*');
